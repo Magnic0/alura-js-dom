a03377580e6d1163c793577e9a96f569bd7ed9e1
Nicolas Medeiros <nicolsoliverg@gmail.com>
2022-07-16 22:00:27 -0300

    JS: assign all data-attributes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const control = document.querySelectorAll('.controle-ajuste');
+const control = document.querySelectorAll('[data-type]');
 
 control.forEach((element) => {
     element.addEventListener('click', (event) => {
-        changeValue(event.target.textContent, event.target.parentNode);
+        changeValue(event.target.dataset.type, event.target.parentNode);
     })
 });
 
 function changeValue(ctrlType, control) {
-    var part = control.querySelector('.controle-contador');
+    var part = control.querySelector('[data-ammount]');
 
     if(ctrlType === '+') {
         part.value = parseInt(part.value) + 1;
