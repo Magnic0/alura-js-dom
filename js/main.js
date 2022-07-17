b603c6bba3c75f25ceb2b263aef9625b43c57467
Nicolas Medeiros <nicolsoliverg@gmail.com>
2022-07-17 00:31:18 -0300

    JS: stats now update in real time

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
 const control = document.querySelectorAll('[data-type]');
+const stats = document.querySelectorAll('[data-stats]');
+const parts = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+};
 
 control.forEach((element) => {
     element.addEventListener('click', (event) => {
         changeValue(event.target.dataset.type, event.target.parentNode);
-    })
+    });
 });
 
 function changeValue(ctrlType, control) {
@@ -13,10 +46,18 @@ function changeValue(ctrlType, control) {
         part.value = parseInt(part.value) + 1;
     } else if(ctrlType === '-') {
         part.value = parseInt(part.value) - 1;
-    }
+    };
 
     if(part.value < 0) {
-        part.value = parseInt(part.value) + 1;
+        part.value = 0;
         return;
-    }
+    };
+
+    updateStats(part.dataset);
+};
+
+function updateStats(part) {
+    stats.forEach(element => {
+        element.textContent = parseInt(element.textContent) + parts[part.ammount][element.dataset.stats];
+    });
 }
